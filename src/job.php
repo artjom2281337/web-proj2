8e60b07514b93d1b0e3d0f11f70d9529701243d3
Artjom Smorgulenko <artjomsmorgulenko@gmail.com>
2025-02-13 16:56:48 +0200

    Update job.php

--- src/job.php
+++ src/job.php
@@ -2,6 +2,20 @@
     $title = "Jobs";
     $css_file = "job";
     include("header.php");
+
+    require 'test/db.php';
+    $sql = "SELECT * FROM jobs";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $job_name = $row['name'];
+            $job_date = $row['startdate'];
+            $job_location = $row['location'];
+            $job_postcode = $row['postcode'];
+        }
+    }
+
 ?>
 
     <article>
@@ -13,9 +27,9 @@
 
         <div class="job-list">
             <div class="jobb">
-                <div class="job-title">Software Engineer</div>
-                <div class="job-date">15.01.2025</div>
-                <div class="job-location">Helsinki Uusimaa, 00400</div>
+                <div class="job-title"><?php echo $job_name;?></div>
+                <div class="job-date"><?php echo $job_date;?></div>
+                <div class="job-location"><?php echo $job_location . " ".$job_postcode;?></div>
             </div>
             <div class="jobb">
                 <div class="job-title">Software Engineer</div>
