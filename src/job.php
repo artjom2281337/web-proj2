90b1c04e4b1d5a2dbe8c4b267e0bb2cc01520fce
Artjom Smorgulenko <artjomsmorgulenko@gmail.com>
2025-02-14 16:56:57 +0200

    Job page now using database
    INSERT INTO jobs (name, startdate, location, postcode) VALUES ('Software Engineer', '2025-01-15', 'Helsinki, Uusimaa', '00400');
    
    INSERT INTO jobs (name, startdate, location, postcode) VALUES ('Designer', '2025-02-10', 'Hämeenlinna', '13100');

--- src/job.php
+++ src/job.php
@@ -6,16 +6,6 @@
     require 'test/db.php';
     $sql = "SELECT * FROM jobs";
     $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $job_name = $row['name'];
-            $job_date = $row['startdate'];
-            $job_location = $row['location'];
-            $job_postcode = $row['postcode'];
-        }
-    }
-
 ?>
 
     <article>
@@ -26,40 +16,24 @@
         </div>
 
         <div class="job-list">
+            <?php 
+            if ($result -> num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $job_name = $row["name"];
+                    $job_date = $row["startdate"];
+                    $job_location = $row["location"];
+                    $job_postcode = $row["postcode"];
+            ?>
             <div class="jobb">
                 <div class="job-title"><?php echo $job_name;?></div>
                 <div class="job-date"><?php echo $job_date;?></div>
-                <div class="job-location"><?php echo $job_location . " ".$job_postcode;?></div>
+                <div class="job-location"><?php echo $job_location. " ". $job_postcode;?></div>
             </div>
-            <div class="jobb">
-                <div class="job-title">Software Engineer</div>
-                <div class="job-date">15.01.2025</div>
-                <div class="job-location">Helsinki Uusimaa, 00400</div>
-            </div>
-            <div class="jobb">
-                <div class="job-title">Software Engineer</div>
-                <div class="job-date">15.01.2025</div>
-                <div class="job-location">Helsinki Uusimaa, 00400</div>
-            </div>
-            <div class="jobb">
-                <div class="job-title">Software Engineer</div>
-                <div class="job-date">15.01.2025</div>
-                <div class="job-location">Helsinki Uusimaa, 00400</div>
-            </div>
-            <div class="jobb">
-                <div class="job-title">Software Engineer</div>
-                <div class="job-date">15.01.2025</div>
-                <div class="job-location">Helsinki Uusimaa, 00400</div>
-            </div>
-            <div class="jobb">
-                <div class="job-title">Software Engineer</div>
-                <div class="job-date">15.01.2025</div>
-                <div class="job-location">Helsinki Uusimaa, 00400</div>
-            </div>
+            <?php }
+            }?>
         </div>
         <div class="load-more">
             <button>Load more</button>
-
         </div>
     </article>
 
